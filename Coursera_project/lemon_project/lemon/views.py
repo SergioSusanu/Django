b4defaa4d9322f82442fbefa41cfd3bfa72051b3
SergioSusanu <Sergio.susanu.developer@gmail.com>
2024-01-08 12:15:07 +0200

    django urls

--- Coursera_project/lemon_project/lemon/views.py
+++ Coursera_project/lemon_project/lemon/views.py
@@ -2,6 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Olla from main project index")
+    dt = datetime.today()
+    if request.method == 'GET':
+        result = 'Main index + Get '
+        if 'name' in  request.GET:
+            result += request.GET['name']
+    result =result + str(dt)
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+
+    msg = f"""
+        <br> Path: {path}
+        <br> Scheme: {scheme}
+        <br> Method: {method}
+        <br> Address: {address}
+        <br> user agent: {user_agent}
+        <br> path info: {path_info}
+        """
+    return HttpResponse(msg)
+
+# from django.views import View
+
+# class index(View):
+#     def get(self, request):
+#         return HttpResponse("Get request")
