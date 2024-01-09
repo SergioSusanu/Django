7a1ca26d04040f55ca52a508904835e959dfd873
SergioSusanu <Sergio.susanu.developer@gmail.com>
2024-01-09 18:41:38 +0200

    starting on models

--- Coursera_project/lemon_project/reservations/models.py
+++ Coursera_project/lemon_project/reservations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    date = models.DateField()
+    guests = models.IntegerField()
+    table = models.IntegerField()
+
+    def __str__(self):
+        return str(self.date) + "  " + str(self.guests)
